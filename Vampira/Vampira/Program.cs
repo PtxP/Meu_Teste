c9d5065aaf28e2b3f6470d391cf4075ed45a42ef
Aleksei da Silva <alekseidasilva@hotmail.com>
2018-05-05 23:25:23 +0100

    primeira atualiação da minha solução

---
+++ Vampira/Vampira/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vampira
+{
+    class Program
+    {
+        int anonascimento;
+        string nome;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, por favor insira o numero");
+            Console.ReadKey();
+        }
+    }
+}
